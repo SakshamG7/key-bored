fbe3b5f39d4216faae41338fda1b2018a2a93373
SakshamG7 <saksham.goel29@icloud.com>
2024-05-09 20:57:35 -0400

    fixed basic cache and and made it use less memory

--- src/main.py
+++ src/main.py
@@ -36,10 +36,11 @@ def printBoard(board, shift: bool = False):
 
 
 # Calculate the distance between two keys on a keyboard.
-def charDistance(board, startKey, endKey) -> int:
-    global boardCache
-    if startKey + " " + endKey in boardCache:
-        return boardCache[startKey + " " + endKey]
+def charDistance(board, startKey, endKey, boardCache: dict) -> int:
+    if startKey == endKey:
+        return 0
+    if startKey + endKey in boardCache:
+        return boardCache[startKey + endKey]
     x1 = -1
     y1 = -1
     x2 = -1
@@ -68,8 +69,8 @@ def charDistance(board, startKey, endKey) -> int:
         x2 += 1
         x2 += (y2 - 1) * AOS
     dist = round(math.dist([x1, y1], [x2, y2]), bitLearning)
-    boardCache[startKey + " " + endKey] = dist
-    boardCache[endKey + " " + startKey] = dist
+    boardCache[startKey + endKey] = dist
+    boardCache[endKey + startKey] = dist
     return dist
 
 
@@ -77,21 +78,22 @@ def charDistance(board, startKey, endKey) -> int:
 def calcDistance(board, text: str, home_keys: list = ["a", "s", "d", "f", "j", "k", "l", ";"], debug: bool = False) -> int:
     distance = 0
     cur_keys = home_keys.copy()
-
+    boardCache = {}
     for letter in text:
         if letter in (" ", "\t", "\n"):
             continue
-        smallest = [1000000, -1]
+        smallest = [1000000000000000, -1]
         for cur_key in range(len(cur_keys)):
-            new_smallest = charDistance(board=board, startKey=home_keys[cur_key], endKey=letter)
+            new_smallest = charDistance(board=board, startKey=home_keys[cur_key], endKey=letter, boardCache=boardCache)
             if new_smallest < smallest[0]:
                 smallest[0] = new_smallest
                 smallest[1] = cur_key
-        smallest[0] = charDistance(board=board, startKey=cur_keys[smallest[1]], endKey=letter)
+        smallest[0] = charDistance(board=board, startKey=cur_keys[smallest[1]], endKey=letter, boardCache=boardCache)
         if debug:
             print(cur_keys[smallest[1]], " finger is now on ", letter, ". Distance: ", smallest[0])
         cur_keys[smallest[1]] = letter
         distance += smallest[0]
+    print(boardCache)
 
     return round(distance, bitLearning)
 
@@ -120,11 +122,10 @@ dvorakBoard = [[['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '[', ']'
 dvorakHomeKeys = list("aoeuhtns")
 # Important global variables
 bitLearning = 5
-boardCache = {}
 
 printBoard(keyboard, shift=True) # Prints the entire QWERTY keyboard
 printBoard(dvorakBoard, shift=True) # Prints the entire Dvorak keyboard
 
 # Calculate the distance of the datatext for both QWERTY and Dvorak
-print(calcDistance(board=keyboard, text=readDatasets(), debug=False))
+print(calcDistance(board=keyboard, home_keys=dvorakHomeKeys, text=readDatasets(), debug=False))
 print(calcDistance(board=dvorakBoard, home_keys=dvorakHomeKeys, text=readDatasets(), debug=False)) # Dvorak is better than QWERTY, lets see if we can improve it further!!!
